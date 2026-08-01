4c95e508ac1093117681242bcaa87b324f1b9820
18rohit_01 <15rohit0505@gmail.com>
2026-08-01 15:11:09 +0530

    first comnit

---
+++ timer.js
@@ -0,0 +1,10 @@
+function timer(){
+// putting it i a function 
+const obj =document.getElementById("root");
+const now = new Date();
+const indianTime = now.toLocaleTimeString();
+obj.innerHTML = indianTime;
+}
+setInterval(timer,1000)
+
+ 
